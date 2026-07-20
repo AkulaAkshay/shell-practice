0f8bce6c557ee643100f70bdb79edefd26d42f43
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 13:33:46 +0530

    passing arguments

--- 08-special_variables.sh
+++ 08-special_variables.sh
@@ -10,3 +10,4 @@ echo "name of the script execution : $0" # dynamically provides the name of the 
 echo "current working directory : $PWD" #provides present working directory
 echo "who is running this: $USER" #who is running this script
 echo "User's home directory: $HOME" # Users home directory
+echo "PID of the script: $$" #when we are executing this script PID will be generated, PID of the scrit 
