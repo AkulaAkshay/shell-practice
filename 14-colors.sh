49bbfcdf09bb3ea8995429c6aadd2286e628145f
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 17:38:08 +0530

    passing arguments

--- 14-colors.sh
+++ 14-colors.sh
@@ -7,5 +7,6 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[37m" #or 0m
 
-echo -e "$G Hello World"
+#echo -e "$G Hello World" # color mentioned here will go to further lines of code also. hence we need to stop it
+echo -e "$G Hello World $N"
 echo "check this color"
