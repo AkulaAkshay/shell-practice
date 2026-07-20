38a11dabe1ab1e6752ef50887c011eab31d9dff5
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 09:59:25 +0530

    some random conversation

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#no space before or after =
+person1=John
+person2=Raheem
+
+echo "$person1: Hi $person2, how are you?"
+echo "$person2: Hello $person1 iam fine, how are you?"
+echo "$person1: iam fine, what are you learning now?"
+echo "$person2: $person1 iam learning shell scripting, what about you?"
