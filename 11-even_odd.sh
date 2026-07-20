6c07c4916b8cf87d9f0c1fbd93c4b9833207ff3e
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 15:13:45 +0530

    passing arguments

--- 11-even_odd.sh
+++ 11-even_odd.sh
@@ -3,7 +3,7 @@
 echo "please provide the number :"
 read NUMBER
 
-if [$(($NUMBER % 2)) -eq 0 ]; then
+if [ $(($NUMBER % 2)) -eq 0 ]; then
     echo "The Number "${NUMBER}" is an even number"
 else
     echo "The Number "${NUMBER}" is an odd number"
