f4a033967022dbc444cfb406081553d3e33f494d
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 10:43:50 +0530

    passing arguments

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1: welcome to Alaska $person2"
+echo "$person2: Thank you $person1, finally not on zoom webibnar"
+echo "$person1: so, do we have a deal?"
+echo "$person2: yes, if u make ukrain joins russia"
