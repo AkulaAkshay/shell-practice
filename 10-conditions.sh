4c612c760e4f4749aa9ca4a7bac57772f4a85ec7
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 14:59:29 +0530

    passing arguments

---
+++ 10-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# lt-less than; gt-grater than; eq-equal to; ne-not equal  to;
+
+if [ $NUMBER -lt 10 ]; then
+   echo "GIVEN Number "${NUMBER}" is less than 10
+else
+   echo "GIVEN Number "${NUMBER}" is grater than or equal to 10
+fi
