36a8e3955ff60f0d5b04de44c9be75cdb852f400
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 09:39:50 +0530

    some random conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin//bash
+
+echo "Ramesh: Hi suresh, how are you?"
+echo "suresh: Hello Ramesh iam fine, how are you?"
+echo "Ramesh: iam fine, what are you learning now?"
+echo "suresh: Ramesh iam learning shell scripting, what about you?"
