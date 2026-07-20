5ab2715058187f55ca1b1e15f64ab1894f82acd1
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 15:58:05 +0530

    passing arguments

--- 12-installation.sh
+++ 12-installation.sh
@@ -5,14 +5,17 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then 
     echo "Error: please run the command with the root privilages"
+    exit 1 # when we have a probability of failure we need to provide exit code as non-zero
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0]; then
     echo "ERROR: Installing MYSQL is failed"
+    exit 1
 else
     echo "Installing MYSQL is success"
+    #exit 0 -- if we want we can prvide but by default it takes 0 only
 fi
 
 #shell scripting biggest dis-advantage is- when there is an error in the script --> it should stop generally, but in shell even if we have 
