b26f3c1366a576897458927017b5fca567848ec6
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 13:24:42 +0530

    passing arguments

--- 08-special_variables.sh
+++ 08-special_variables.sh
@@ -5,3 +5,5 @@ echo "All the variables passed to the script: $*"
 #we did not pass any variable value for * and @, but still it gives the variable values we passed in the command line,
 #these are called special variables i.e., by default the variables given by the shell itself we are not creating any values for these 
 #special variables
+
+echo "name of the script execution : $0" # dynamically provies the name of the script
