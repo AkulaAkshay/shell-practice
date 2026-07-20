73305a12f627746151001b4869db53d2157bf688
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 11:31:35 +0530

    passing arguments

---
+++ 06-env-variables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#we did not pass the variable name as read command, we din't pass the variables here also, we din't pass the var even as arguments also
+echo "Course name is: $COURSE"
