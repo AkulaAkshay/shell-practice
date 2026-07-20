11dd6b946de989661cfcc23f3fb20aa0ec8bb898
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 13:54:20 +0530

    passing arguments

---
+++ 09-data-types.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#There are no explcit dta types in shell, usually shell will consider every thing as string initially, but when we are using that data type 
+# shell behaves intelligent and adapt accoridng to the given data type
+
+N1=100
+N2=200
+
+SUM=$((N1+N2))
+echo "sum of two numbers is: ${SUM}"
