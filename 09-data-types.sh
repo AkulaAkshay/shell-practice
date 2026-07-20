31443a98c1a2a5a6f05e14d9a8a081007491361c
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 14:34:44 +0530

    passing arguments

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -12,8 +12,10 @@ echo "sum of two numbers is: ${SUM}"
 
 #-----------------------------------
 #ARRAY - list of string or numbers
-LEADERS=("modi", "putin", "trump")
+LEADERS=("modi", "putin", "trump") # size of array=3, max index=2
 
 echo "ALL Leaders: ${LEADERS[@]}"
 echo "ALL Leaders: ${LEADERS[*]}"
-echo "ALL Leaders: ${LEADERS[1]}"
+echo "first Leader: ${LEADERS[0]}"
+echo "second Leader: ${LEADERS[1]}"
+echo "second Leader: ${LEADERS[10]}" #it dosen't throw any error in shell,but in other programming language it throws error-index out of range
