d17196f152ffc3a7926351190b924099ec955b60
Akula Akshay <akshayakula13@gmail.com>
2026-07-20 13:27:28 +0530

    passing arguments

--- 08-special_variables.sh
+++ 08-special_variables.sh
@@ -4,6 +4,7 @@ echo "All the variables passed to the script: $@"
 echo "All the variables passed to the script: $*"
 #we did not pass any variable value for * and @, but still it gives the variable values we passed in the command line,
 #these are called special variables i.e., by default the variables given by the shell itself we are not creating any values for these 
-#special variables
+#special variables [for every tool we have some special variables like jenkins,etc.,]
 
-echo "name of the script execution : $0" # dynamically provies the name of the script
+echo "name of the script execution : $0" # dynamically provides the name of the script
+echo "current working directory : $PWD" #provides present working directory
