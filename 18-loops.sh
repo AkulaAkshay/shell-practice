ced1f64e203d309d93c314012337e4f3d748f6b9
Akula Akshay <akshayakula13@gmail.com>
2026-07-21 09:34:06 +0530

    passing arguments

---
+++ 18-loops.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#we are installing 3 packages - mongodb, ngix, python3
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m" #or 0m
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #full path - /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "script started executed at : $(date)" | tee -a $LOG_FILE #tee is used because by echo statemnet it prints in the terminal but dosen't
+# store in the logs, in order to print in the terminal and store in the logs we use tee command.
+
+if [ $USERID -ne 0 ]; then 
+    echo "Error: please run the command with the root privilages"
+    exit 1 # when we have a probability of failure we need to provide exit code as non-zero
+fi
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+      echo -e "ERROR: Installing $2 is $R failed $N" | tee -a $LOG_FILE
+      exit 1
+    else
+      echo -e "Installing $2 is $G success $N" | tee -a $LOG_FILE
+      #exit 0 -- if we want we can prvide but by default it takes 0 only
+    fi
+
+}
+
+
+# $@ or $* - to display all the arguments
+
+for i in $*
+do
+   echo "package is : $i"
+   echo "package $i is installed"
+done
