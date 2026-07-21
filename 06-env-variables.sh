69030cf952aedf6f91e3645b1b6087f1b60e229a
Akula Akshay <akshayakula13@gmail.com>
2026-07-21 08:56:01 +0530

    passing arguments

--- 06-env-variables.sh
+++ 06-env-variables.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #we did not pass the variable name as read command, we din't pass the variables here also, we din't pass the var even as arguments also
+# we will pass the variable in the ~/.bashrh file 
 echo "Course name is: $COURSE"
