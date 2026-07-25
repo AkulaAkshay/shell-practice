45a6cab4cf677c725c0c703a6aee26f4d41cfc03
Akula Akshay <akshayakula13@gmail.com>
2026-07-25 19:54:04 +0530

    some related info

--- 19-set.sh
+++ 19-set.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+
+#here even though we have an error, both the statements 1,2 will be executed because by default the shell archicheture is designed in such  a way that whole code will be exexuted irrespective of any error present, that is the reason in shell-roboshop for every line we execute, we use VALIDATE function. AND we have something called set if we use this we can stop the execution of the code whenever we see an error
+
+#echo "HELLO"
+#echo "statement1: Before error statements"
+#kjnjnjlkdsa
+#echo "statement2: After error statements"
+
 echo "HELLO"
-echo "Before error statements"
+echo "statement1: Before error statements"
 kjnjnjlkdsa
-echo "After error statements"
+echo "statement2: After error statements"
+
+
